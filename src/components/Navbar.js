f129ed0f08eaefdd4e1aa10c32be549e320b3fa7
janeanekim <jmk90020@gmail.com>
2022-09-12 07:00:42 -0700

    homepage structure

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,33 +4,33 @@ import { Link } from "react-router-dom"
 import '../App.css';
 const Navbar = () => {
     return (
-        <nav class='navbar'>
+        <nav className='navbar'>
          
-                    <nav class="navbar navbar-expand-lg navbar-light bg-white">
-                    <a class="navbar-brand">
+                    <nav className="navbar navbar-expand-lg navbar-light bg-white">
+                    <a className="navbar-brand">
                     </a>
                         
-                    <Link class="navigate" to= '/'>Home</Link>
+                    <Link className="navigate" to= '/'>Home</Link>
 
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link">
-                        <Link class="navigate" to='/NeoRhythm'>NeoRhythm </Link>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                <li className="nav-item active">
+                    <a className="nav-link">
+                        <Link className="navigate" to='/NeoRhythm'>NeoRhythm </Link>
                     </a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link">
-                    <Link class="navigate" to='/NeoWrap'>NeoWrap</Link>
+                <li className="nav-item">
+                    <a className="nav-link">
+                    <Link className="navigate" to='/NeoWrap'>NeoWrap</Link>
                     </a>
             
 
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Pricing</a>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Pricing</a>
                 </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Disabled</a>
+                <li className="nav-item">
+                    <a className="nav-link" href="#">Disabled</a>
                 </li>
                 </ul>
             </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,8 @@ import Navbar from '../components/Navbar';
 import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Image from 'react-bootstrap/Image';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../App.css';
 import SakuraKeyboard from '../Images/SakuraKeyboard.jpg';
@@ -18,78 +20,90 @@ const Home = () => {
         
         </><Container fluid ="xl">
                 <Row>
-                    <Card style={{ width: '18rem' }}>
-                    <Link to="NeoRhythm">
-                          <img class="NeoRhythm" src="https://omnipemf.com/wp-content/uploads/2022/02/NeoRhytm_9x9.png" alt="NeoRhythm" />
+                    <Col>
+                        <Card>
+                        <Link to="NeoRhythm">
+                            <img class="NeoRhythm" src="https://omnipemf.com/wp-content/uploads/2022/02/NeoRhytm_9x9.png" alt="NeoRhythm" />
 
-                    </Link>
-                        <Card.Body>
-                            <Card.Title>NeoRhythm</Card.Title>
-                            <Card.Text>
-                                <div class="Neo"></div>$299.99
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card style={{ width: '18rem' }}>
-                  <Link to= "NeoWrap">
-                         <img class= "NeoWrap" src="https://omnipemf.com/wp-content/uploads/2022/03/wrap.pngholder.js/100px180" alt="NeoWrap" /> 
-                  </Link>
-                        <Card.Body>
-                            <Card.Title>NeoWrap</Card.Title>
-                            <Card.Text>
-                               <div class= "Neo2"></div> $49.00
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
+                        </Link>
+                            <Card.Body>
+                                <Card.Title>NeoRhythm</Card.Title>
+                                <Card.Text>
+                                    <div class="Neo"></div>$299.99
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card>
+                        <Link to= "NeoWrap">
+                                <img class= "NeoWrap" src="https://omnipemf.com/wp-content/uploads/2022/03/wrap.pngholder.js/100px180" alt="NeoWrap" /> 
+                        </Link>
+                                <Card.Body>
+                                    <Card.Title>NeoWrap</Card.Title>
+                                    <Card.Text>
+                                    <div class= "Neo2"></div> $49.00
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+                    </Col>
+                    <Col>
+                        <Card >
+                            <Card.Img variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
                 </Row>
 
                 <br />
 
                 <Row >
-                    <Card style={{ width: '18rem' }}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
-                        <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
-                            <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                    <Card style={{ width: '18rem' }}>
-                        <Link to="keychron_k8_keyboard" style={{ color:"black", textDecoration: 'none' }} >
-                        <Card.Img variant="top" src={Keychron} />
-                        <br/>
-                        <Card.Body>
-                            <Card.Title>Keychron K8 Keyboard</Card.Title>
-                            <Card.Text>
-                                $100.00
-                            </Card.Text>
-                        </Card.Body>
-                        </Link>
-                    </Card>
-                    <Card style={{ width: '18rem' }}>
-                        <Link to="va88m_sakura_keyboard" style={{ color:"black", textDecoration: 'none' }} >
-                        <Card.Img variant="top" src={SakuraKeyboard} />
-                        <br/>
-                        <Card.Body>
-                            <Card.Title>Varmillo Sakura Edition Keyboard</Card.Title>
-                            <Card.Text>
-                                $150.00
-                            </Card.Text>
-                        </Card.Body>
-                        </Link>
-                    </Card>
+                    <Col>
+                        <Card >
+                            <Card.Img className="NeoRhythm" variant="top" src="holder.js/100px180" />
+                            <Card.Body>
+                                <Card.Title>Card Title</Card.Title>
+                                <Card.Text>
+                                    Some quick example text to build on the card title and make up the
+                                    bulk of the card's content.
+                                </Card.Text>
+                            </Card.Body>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card >
+                            <Link to="keychron_k8_keyboard" style={{ color:"black", textDecoration: 'none' }} >
+                            <Image className="product-card" variant="top" src={Keychron} fluid={true} />
+                            <br/>
+                            <Card.Body>
+                                <Card.Title>Keychron K8 Keyboard</Card.Title>
+                                <Card.Text>
+                                    $100.00
+                                </Card.Text>
+                            </Card.Body>
+                            </Link>
+                        </Card>
+                    </Col>
+                    <Col>
+                        <Card >
+                            <Link to="va88m_sakura_keyboard" style={{ color:"black", textDecoration: 'none' }} >
+                            <Image className="product-card" variant="top" src={SakuraKeyboard} fluid={true}/>
+                            <br/>
+                            <Card.Body>
+                                <Card.Title>Varmillo Sakura Edition Keyboard</Card.Title>
+                                <Card.Text>
+                                    $150.00
+                                </Card.Text>
+                            </Card.Body>
+                            </Link>
+                        </Card>
+                    </Col>
                 </Row>
             </Container></>
 
