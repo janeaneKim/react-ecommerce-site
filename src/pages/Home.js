2b6b3349f08a831fb8da432290ad785ebef84f5a
janeanekim <jmk90020@gmail.com>
2022-09-13 07:12:50 -0700

    homepage formatting

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, Outlet } from "react-router-dom";
 import Navbar from '../components/Navbar';
-import Card from 'react-bootstrap/Card';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
@@ -57,52 +56,8 @@ const Home = () => {
     ]);
 
     let productArray = productList.map((product) => 
-        <li key={product.id}>
-            <ProductCard info={product} />
-        </li>
+            <ProductCard key={product.id} info={product} />
     )
-
-
-    let NeoRhythm = {
-        id: 1,
-        link: "NeoRhythm",
-        title: "NeoRhythm",
-        image: ( <img class="product-card" src="https://omnipemf.com/wp-content/uploads/2022/02/NeoRhytm_9x9.png" alt="NeoRhythm" />),
-        price: (<div class="Neo">$299.99</div>)
-    }
-
-    let NeoWrap = {
-        id: 2,
-        link: "NeoWrap",
-        title: "NeoWrap",
-        image: (<img class= "product-card" src="https://omnipemf.com/wp-content/uploads/2022/03/wrap.pngholder.js/100px180" alt="NeoWrap" /> ),
-        price: (<div class= "Neo2">$49.00</div> )
-    }
-
-    let HardDriveDock = {
-        id: 3,
-        link: "HardDriveDock",
-        title: "Hard Drive Dock",
-        image: (<img class="HardDriveDock product-card" src="/Images/HardDriveDock.jpg" alt="Hard Drive Dock" /> ),
-        price: (<div class="HardDriveDock">$25.99</div>)
-    }
-
-    let k8 ={
-        id: 5,
-        link: "keychron_k8_keyboard",
-        title: "Keychron K8 Keyboard",
-        image: ( <Image className="product-card" variant="top" src="../Images/Keychron.webp" fluid={true} /> ),
-        price: (<div>$100.00</div> )
-    }
-
-    let sakura ={
-        id: 6,
-        link: "va88m_sakura_keyboard",
-        title: "Varmillo Sakura Edition Keyboard",
-        image: ( <Image className="product-card" variant="top" src="../Images/SakuraKeyboard.jpg" fluid={true}/> ),
-        price: (<div>$150.00</div> )
-    }
-
 
     return (
         <><>
@@ -112,43 +67,30 @@ const Home = () => {
             </section>
         
         </><Container fluid ="xl">
-            <div>
-                {productArray}
-            </div>
-                {/* <Row>
+                <Row>
                     <Col md>
-                        <ProductCard  info={NeoRhythm}/>
+                        {productArray[0]}
                     </Col>
                     <Col md>
-                        <ProductCard info={NeoWrap} />
+                        {productArray[1]}   
                     </Col>
                     <Col md>
-                        <ProductCard info={HardDriveDock} />
+                        {productArray[2]}
                     </Col>
                 </Row>
-
                 <br />
 
                 <Row >
                     <Col md>
-                        <Card >
-                            <Card.Img className="NeoRhythm" variant="top" src="holder.js/100px180" />
-                            <Card.Body>
-                                <Card.Title>Card Title</Card.Title>
-                                <Card.Text>
-                                    Some quick example text to build on the card title and make up the
-                                    bulk of the card's content.
-                                </Card.Text>
-                            </Card.Body>
-                        </Card>
+                        {productArray[3]}
                     </Col>
                     <Col md>
-                        <ProductCard  info={k8}/>
+                        {productArray[4]}
                     </Col>
                     <Col md>
-                        <ProductCard info={sakura} />
+                        {productArray[5]}
                     </Col>
-                </Row> */}
+                </Row>
             </Container></>
 
 
